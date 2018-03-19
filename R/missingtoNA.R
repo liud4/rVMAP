b6fc07939a964cb4ea397ee160fc93d68476a644
Omair A. Khan <omair.adil.khan@gmail.com>
2018-03-19 16:03:55 -0500

    updated project name after merge to liud4; redesigned preprocessing.funs.R and renamed it to missingtoNA.R for consistency

---
+++ R/missingtoNA.R
@@ -0,0 +1,99 @@
+#' A preprocessing function to replace values with NA.
+#'
+#' @param vec A numeric, integer, or character vector.
+#' @param mod.val A single number that divides into the target values with zero remainder.
+#' @param equal.val A vector of length 1 or greater containing any combination of numbers, characters, or strings that match the target values exactly.
+#' @return The input vector with all matching values set to missing.
+#' @examples
+#' test.df <- data_frame(a = 1:5, b = 2:6, c = rnorm(5), d = LETTERS[1:5])
+#'
+#' missingtoNA(test.df$b, mod.val = 3)
+#'
+#' test.df %>% mutate_all(
+#'   ~ missingtoNA(., equal.val = 2)
+#' )
+#'
+#' test.df %>% mutate_if(
+#'  ~ any(class(.) %in% c("numeric", "integer", "character")),
+#'  ~ missingtoNA(., equal.val = c(2, 3))
+#' )
+#'
+#' test.df %>% mutate_at(
+#'   c("a", "b"),
+#'   ~ missingtoNA(., mod.val = 2, equal.val = c(2, 3, "E"))
+#' )
+#'
+#' epoch1.main.df %>% mutate_at(
+#'   grep("^ecogself\\_(mem|lang|vis|plan|org|attn)\\d{2}$", names(.)),
+#'    ~ missingtoNA(., equal.val = 0)
+#' )
+#'
+#' @export
+
+missingtoNA <- function(vec, mod.val, equal.val) {
+  # output error if key arguments missing
+  if (missing(vec)) {
+    stop("Please specify a vector of data.")
+  }
+  if (missing(mod.val) & missing(equal.val)) {
+    stop("Please specify at least one argument to match.")
+  }
+
+  if (missing(mod.val) & !missing(equal.val)) { # equal.val only
+    if (any(class(vec) == "numeric")) {
+      case_when(
+        vec %in% equal.val ~ NA_real_,
+        TRUE ~ vec
+      )
+    } else if (any(class(vec) == "integer")) {
+      case_when(
+        vec %in% equal.val ~ NA_integer_,
+        TRUE ~ vec
+      )
+    } else if (any(class(vec) == "character")) {
+      case_when(
+        as.character(vec) %in% equal.val ~ NA_character_,
+        TRUE ~ vec
+      )
+    } else {
+      warning("Vector must be of class numeric, integer, or character.")
+    }
+  } else if (!missing(mod.val) & missing(equal.val)) { # mod.val only
+  if (any(class(vec) == "numeric")) {
+    case_when(
+      vec %% mod.val == 0 ~ NA_real_,
+      TRUE ~ vec
+    )
+    } else if (any(class(vec) == "integer")) {
+      case_when(
+        vec %% mod.val == 0 ~ NA_integer_,
+        TRUE ~ vec
+      )
+    } else {
+      # OAK 20180319 : cannot do division on character class
+      warning("Vector must be of class numeric or integer.")
+    }
+  } else { # both equal.val and mod.val
+    if (any(class(vec) == "numeric")) {
+      case_when(
+        vec %% mod.val == 0 ~ NA_real_,
+        vec %in% equal.val ~ NA_real_,
+        TRUE ~ vec
+      )
+    } else if (any(class(vec) == "integer")) {
+      case_when(
+        vec %% mod.val == 0 ~ NA_integer_,
+        vec %in% equal.val ~ NA_integer_,
+        TRUE ~ vec
+      )
+    } else if (any(class(vec) == "character")) {
+      case_when(
+        # OAK 20180319 : cannot do division on character class
+        vec %in% equal.val ~ NA_character_,
+        TRUE ~ vec
+      )
+    } else {
+      warning("Vector must be of class numeric, integer, or character.")
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
-rVMAC
+rVMAP
 =====
 
-The rVMAC package is intended to improve the efficiency and reproducibility of collaborations between [VUMC Department of Biostatistics](http://biostat.mc.vanderbilt.edu/) and the [Vanderbilt Memory and Alzheimer's Center](https://www.vumc.org/vmac/). rVMAC is intended for internal use and contains the following:
+The rVMAP package is intended to improve the efficiency and reproducibility of collaborations between [VUMC Department of Biostatistics](http://biostat.mc.vanderbilt.edu/) and the [Vanderbilt Memory and Alzheimer's Center](https://www.vumc.org/vmac/) for Vanderbilt Memory and Aging Project studies. rVMAP is intended for internal use and contains the following:
 
 -   functions that are commonly used in statistical analysis in VMAC research
 -   scripts that will help automate mundane tasks (e.g. quarterly [data freeze](./demo/data-freeze.R) and [data merge](./demo/data-merge.nb.html))
@@ -13,11 +13,11 @@ The rVMAC package is intended to improve the efficiency and reproducibility of c
 Installation
 ------------
 
-You can install rVMAC from github with:
+You can install rVMAP from github with:
 
 ``` r
 # install.packages("devtools")
-devtools::install_github("prettynumbers/rVMAC")
+devtools::install_github("liud4/rVMAP")
 ```
 
 <!-- ## Example -->
