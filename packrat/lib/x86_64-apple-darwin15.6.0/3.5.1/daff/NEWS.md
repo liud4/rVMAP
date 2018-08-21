a5f1d16db0cca38860bbc2b6f27300768fd31101
Omair A. Khan <omair.adil.khan@gmail.com>
2018-08-21 16:14:23 -0500

    added all necessary packages to packrat

---
+++ packrat/lib/x86_64-apple-darwin15.6.0/3.5.1/daff/NEWS.md
@@ -0,0 +1,22 @@
+0.3.0
+- Lots of improvements thanks to @gwarnes-mdsol
+- 'render_diff' now defaults to 'pretty=TRUE'.
+- Use inline CSS instead of obsolete '<center>' tag.
+- Improve Output Formatting and Add Support for Daff Flags
+- Update daff.js to version 1.3.25.
+
+0.2.1
+- added summary for patch, thanks to @jsta, feature request #12
+
+0.2.0
+- updated daff.js library to 1.3.16, thanks to @jeroenooms, solves issue #9
+
+0.1.3
+- changed internal I() into JS() thanks to @jeroenooms
+
+0.1.2
+- fixed issue #5 handling NA's, thanks to @eusebe
+- removed the R version dependency (is dependent on V8)
+
+0.1.1
+- initial version
---
+++ packrat/lib/x86_64-apple-darwin15.6.0/3.5.1/daff/js/util.js
@@ -0,0 +1,79 @@
+function to_table_view(table, names){
+  var view = table;
+  if (names){
+    view.unshift(names);
+  }
+  return new daff.TableView(view);
+}
+
+function to_array(table){
+  var view = [];
+  if (table.length === 0){
+    return view;
+  }
+  view.push(_.keys(table[0]));
+  for (var i=0; i < table.length; i++){
+    view.push(_.values(table[i]));
+  }
+  return view;
+}
+
+/* x: TableView.data */
+function to_objects(x){
+  var keys = x[0];
+  var result = [];
+  for (var i=1; i < x.length; i++){
+    result.push(_.object(keys, x[i]));
+  }
+  return result;
+}
+
+function diff(src, target, flags){
+  var alignment = daff.compareTables(src, target).align();
+  var highlighter = new daff.TableDiff(alignment, flags);
+  var data_diff = [];
+  var table_diff = new daff.TableView(data_diff);
+  highlighter.hilite(table_diff);
+  table_diff.summary = highlighter.getSummary()
+  return table_diff;
+}
+
+function to_csv(table){
+  var csv = new daff.Csv();
+  return csv.renderTable(table);
+}
+
+function from_csv(txt){
+  var csv = new daff.Csv();
+  var output = new daff.SimpleTable(0,0);
+	csv.parseTable(txt,output);
+	if(output !== null) output.trimBlank();
+	return output;
+}
+
+function patch_data(table, patch){
+  var patcher = new daff.HighlightPatch(table, patch);
+  patcher.apply();
+  return table;
+}
+
+function merge_data(parent, a, b){
+  var flags = new daff.CompareFlags();
+	var merger = new daff.Merger(parent,a,b,flags); //add flags?
+	var conflicts = merger.apply();
+  return {
+    conflicts: conflicts,
+    ok: conflicts === 0
+  };
+}
+
+function render_diff(diff, fragment, pretty){
+  fragment = !!fragment;
+  pretty = !!pretty;
+
+  var renderer = new daff.DiffRender();
+	renderer.usePrettyArrows(pretty);
+	renderer.render(diff);
+	if(!fragment) renderer.completeHtml();
+	return renderer.html();
+}
