dee5afc4cffb13b73140ec1744f248947433dc1f
Omair A. Khan <omair.adil.khan@gmail.com>
2018-11-26 15:02:50 -0600

    added code to process calculated fields. this is now called in process_raw_data.R. resolves https://github.com/liud4/rVMAP/issues/9

---
+++ R/process_calculated_fields.R
@@ -0,0 +1,45 @@
+process_calculated_fields <- function(data, data_label, epoch = current_epoch) {
+  current_data.df <- data
+
+  if (!grepl("\\.static$", data_label)) {
+    metadata.df <- MAPfreeze.list[[epoch]][["metadata"]][[data_label]] %>%
+      filter(
+        field_type == "calc"
+      ) %>%
+      select(
+        c("field_name", "select_choices_or_calculations")
+      ) %>%
+      as_tibble()
+
+    metadata.df <- ifelse(dim(metadata.df)[1] > 0, metadata.df, NULL)
+
+    if (!is.null(metadata.df)) {
+      metadata.df$element_variables <- purrr::map(  # extract variables used in calculation of this variable
+        metadata.df$field_name,
+        function(x)
+          gsub(
+            "\\[|\\]",
+            "",
+            stringr::str_extract_all(
+              metadata.df[metadata.df$field_name == x, "select_choices_or_calculations"],
+              "\\[([^\\[\\]]+)\\]")[[1]]
+          )
+      )
+
+      #current_data.df <- MAPfreeze.list[[epoch]][["data"]][[data_label]]
+
+      for (redcap_calculated_variables.i in dim(metadata.df)[1]) {
+        element_variables <- pull(metadata.df[redcap_calculated_variables.i, "element_variables"])[[1]]
+        for (row.i in dim(current_data.df)[1]) {
+          current_data.df[row.i, current.var] <- ifelse(
+            any(is.na(current_data.df[row.i, element_variables])),
+            NA,
+            current_data.df[row.i, current.var]
+          )
+        }
+      }
+    }
+  }
+
+  return(current_data.df)
+}
