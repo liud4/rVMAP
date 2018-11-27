6e34e11c317ee4219f2598d83228df6d5ee9c53b
Omair A. Khan <omair.adil.khan@gmail.com>
2018-11-27 11:57:28 -0600

    corrected an error caused by having "MAPfreeze.list = MAPfreeze.list" now the argument is "data_freeze"

--- R/consolidate_variable_names_and_labels.R
+++ R/consolidate_variable_names_and_labels.R
@@ -1,23 +1,24 @@
-consolidate_variable_names_and_labels <- function(MAPfreeze.list = MAPfreeze.list) {
+consolidate_variable_names_and_labels <- function(data_freeze = MAPfreeze.list) {
   within_epoch.df <- NULL
   all_variables_labels.df <- NULL
 
-  for (epoch.i in names(MAPfreeze.list)[-1]) {
-    for (dataset.i in names(MAPfreeze.list[[epoch.i]][["data"]])) {
+  for (epoch.i in names(data_freeze)[-1]) {
+    for (dataset.i in names(data_freeze[[epoch.i]][["data"]])) {
       my.df <- NULL
+
       if (grepl("\\.static", dataset.i)) {
         my.df <- dplyr::tibble(
           epoch = epoch.i,
           dataset = dataset.i,
-          field_name = names(MAPfreeze.list[[epoch.i]][["data"]][[dataset.i]]),
+          field_name = names(data_freeze[[epoch.i]][["data"]][[dataset.i]]),
           field_label = NA_character_
         )
       } else {
         my.df <- dplyr::tibble(
           epoch = epoch.i,
           dataset = dataset.i,
-          field_name = MAPfreeze.list[[epoch.i]][["metadata"]][[dataset.i]] %>% pull(field_name),
-          field_label = MAPfreeze.list[[epoch.i]][["metadata"]][[dataset.i]] %>% pull(field_label)
+          field_name = data_freeze[[epoch.i]][["metadata"]][[dataset.i]] %>% pull(field_name),
+          field_label = data_freeze[[epoch.i]][["metadata"]][[dataset.i]] %>% pull(field_label)
         )
       }
       within_epoch.df <- rbind(within_epoch.df, my.df)
