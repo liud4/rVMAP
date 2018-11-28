563cf0090863a1892028b44d31b1feb53774772b
Omair A. Khan <omair.adil.khan@gmail.com>
2018-11-28 12:44:52 -0600

    committed various small changes: format_id.R now explicitly converts to character; process_calculated_fields.R has the ifelse() which was causing errors replaced with if(); skeleton.Rmd has updated wording; .gitignore has more files to ignore.

--- R/process_calculated_fields.R
+++ R/process_calculated_fields.R
@@ -1,6 +1,4 @@
 process_calculated_fields <- function(data, data_label, epoch = current_epoch) {
-  current_data.df <- data
-
   if (!grepl("\\.static$", data_label)) {
     metadata.df <- MAPfreeze.list[[epoch]][["metadata"]][[data_label]] %>%
       filter(
@@ -28,16 +26,16 @@ process_calculated_fields <- function(data, data_label, epoch = current_epoch) {
           )
       )
 
-      #current_data.df <- MAPfreeze.list[[epoch]][["data"]][[data_label]]
+      # current_data.df <- MAPfreeze.list[[epoch]][["data"]][[data_label]]
+      current_data.df <- data
 
-      for (redcap_calculated_variables.i in dim(metadata.df)[1]) {
-        element_variables <- pull(metadata.df[redcap_calculated_variables.i, "element_variables"])[[1]]
+      for (redcap_calculated_variables.i in 1:dim(metadata.df)[1]) {
+        current.var <- pull(metadata.df[redcap_calculated_variables.i, "field_name"])
+        current_components.var <- pull(metadata.df[redcap_calculated_variables.i, "element_variables"])[[1]]
         for (row.i in dim(current_data.df)[1]) {
-          current_data.df[row.i, current.var] <- ifelse(
-            any(is.na(current_data.df[row.i, element_variables])),
-            NA,
-            current_data.df[row.i, current.var]
-          )
+          if (any(is.na(current_data.df[row.i, current_components.var]))) {
+            current_data.df[row.i, current.var] <- NA
+          }
         }
       }
     }
