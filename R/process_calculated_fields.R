bacbbfc2809dc66586ace7bd4c686b48753f4b10
Omair A. Khan <omair.adil.khan@gmail.com>
2018-12-17 14:42:04 -0600

    corrected typo in process_calculated_fields.R that prevented innermost for loop from running properly.

--- R/process_calculated_fields.R
+++ R/process_calculated_fields.R
@@ -28,13 +28,10 @@ process_calculated_fields <- function(data, data_label, epoch = current_epoch) {
           )
       )
 
-      # current_data.df <- MAPfreeze.list[[epoch]][["data"]][[data_label]]
-      # current_data.df <- data
-
       for (redcap_calculated_variables.i in 1:dim(metadata.df)[1]) {
         current.var <- pull(metadata.df[redcap_calculated_variables.i, "field_name"])
         current_components.var <- pull(metadata.df[redcap_calculated_variables.i, "element_variables"])[[1]]
-        for (row.i in dim(current_data.df)[1]) {
+        for (row.i in 1:dim(current_data.df)[1]) {
           if (any(is.na(current_data.df[row.i, current_components.var]))) {
             current_data.df[row.i, current.var] <- NA
           }
