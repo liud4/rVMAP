2b8730038df13e1330a7e77fdd495e8b06de7a73
Omair A. Khan <omair.adil.khan@gmail.com>
2019-04-25 15:41:05 -0500

    Updated McEvoy AD Signature derivation with approach based on NC sample only.

--- R/derive_AD_signature.R
+++ R/derive_AD_signature.R
@@ -50,66 +50,123 @@ derive_AD_signature <- function(data) {
   myfit <- paste0("fit.", 1:length(unique(mcevoy.Y)))
   mcevoy.all.temp <- NULL
 
-  #for (i in seq_along(unique(data$epoch))) { # OAK 20180823: Once Epoch 4 data is available, this should be uncommented.
-  for (i in 1:4) {
-
-    epoch.df <- data %>%
-      dplyr::filter(epoch == i) %>%
-      select(
-        one_of(
-          Cs(map.id, epoch, age, sex.factor, intracranialvol, avg.hippocampus, avg.entorhinal.thickness,
-             avg.middletemporal.thickness, avg.bankssts.thickness, avg.isthmuscingulate.thickness,
-             avg.superiortemporal.thickness, avg.medialorbitofrontal.thickness, avg.lateralorbitofrontal.thickness)
-        )
+  derive_mcevoy.df <- data %>%
+    dplyr::filter(epoch == 1) %>%
+    dplyr::filter(diagnosis.factor.base == "Normal") %>%
+    dplyr::select(
+      one_of(
+        Cs(map.id, epoch, age, sex.factor, intracranialvol, avg.hippocampus, avg.entorhinal.thickness,
+           avg.middletemporal.thickness, avg.bankssts.thickness, avg.isthmuscingulate.thickness,
+           avg.superiortemporal.thickness, avg.medialorbitofrontal.thickness, avg.lateralorbitofrontal.thickness)
       )
-
-    epoch.df <- droplevels(epoch.df)
-
-    mcevoy.temp <- data.frame(
-      map.id = epoch.df$map.id,
-      epoch = epoch.df$epoch,
-      AD.sig.mcevoy = 0,
-      stringsAsFactors = FALSE
     )
 
-    fit.1 <- lm(formula = avg.hippocampus ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
-    fit.2 <- lm(formula = avg.entorhinal.thickness ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
-    fit.3 <- lm(formula = avg.middletemporal.thickness ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
-    fit.4 <- lm(formula = avg.bankssts.thickness ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
-    fit.5 <- lm(formula = avg.isthmuscingulate.thickness ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
-    fit.6 <- lm(formula = avg.superiortemporal.thickness ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
-    fit.7 <- lm(formula = avg.medialorbitofrontal.thickness ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
-    fit.8 <- lm(formula = avg.lateralorbitofrontal.thickness ~ age + sex.factor + intracranialvol,
-                data = epoch.df,
-                na.action = na.exclude)
+  derive_mcevoy.df <- droplevels(derive_mcevoy.df)
 
-    for (i in 1:length(mcevoy.coef)) {
-      mcevoy.temp[, "AD.sig.mcevoy"] <- mcevoy.temp[, "AD.sig.mcevoy"] + (rstandard(get(myfit[i])) * mcevoy.coef[i])
-    }
+  fit.1 <- lm(formula = avg.hippocampus ~ age + sex.factor + intracranialvol,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+  #     (Intercept)               age  sex.factorFemale   intracranialvol
+  #       2908.6376          -24.0563          104.2237            0.0013
+  # SD Residuals: 314.2
 
-    mcevoy.all.temp <- rbind(mcevoy.all.temp, mcevoy.temp)
-  }
+  fit.2 <- lm(formula = avg.entorhinal.thickness ~ age + sex.factor,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+  #      (Intercept)               age  sex.factorFemale
+  #          3.46652          -0.00924           0.00237
+  # SD Resid: 0.3032
 
-  data <- merge(data, mcevoy.all.temp, by = c("map.id", "epoch"), all.x = TRUE)
+  fit.3 <- lm(formula = avg.middletemporal.thickness ~ age + sex.factor,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+  #     (Intercept)               age  sex.factorFemale
+  #          2.8533           -0.0058            0.0800
+  # SD Resid: 0.1278
+
+  fit.4 <- lm(formula = avg.bankssts.thickness ~ age + sex.factor,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+  # (Intercept)               age  sex.factorFemale
+  # 2.38087          -0.00229           0.08601
+  # SD Resid: 0.1807
+
+  fit.5 <- lm(formula = avg.isthmuscingulate.thickness ~ age + sex.factor,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+
+  # (Intercept)               age  sex.factorFemale
+  # 2.92362          -0.00829           0.07777
+  # SD Resid: 0.138
+
+  fit.6 <- lm(formula = avg.superiortemporal.thickness ~ age + sex.factor,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+
+  #     (Intercept)               age  sex.factorFemale
+  #    5045.0             -24.6            -209.8
+  # SD RESID: 354.1
+
+  fit.7 <- lm(formula = avg.medialorbitofrontal.thickness ~ age + sex.factor,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+
+  #     (Intercept)               age  sex.factorFemale
+  #    2.283472          0.000498          0.060299
+  # SD RESID: 0.1437
+
+  fit.8 <- lm(formula = avg.lateralorbitofrontal.thickness ~ age + sex.factor,
+              data = derive_mcevoy.df,
+              na.action = na.exclude)
+  #      (Intercept)               age  sex.factorFemale
+  #     2.65155          -0.00525           0.08718
+  # SD RESID: 0.1133
+
+
+  data_w_fit.df <- data %>%
+    modelr::add_predictions(model = fit.1, var = "mcevoy.fit.1") %>%
+    modelr::add_predictions(model = fit.2, var = "mcevoy.fit.2") %>%
+    modelr::add_predictions(model = fit.3, var = "mcevoy.fit.3") %>%
+    modelr::add_predictions(model = fit.4, var = "mcevoy.fit.4") %>%
+    modelr::add_predictions(model = fit.5, var = "mcevoy.fit.5") %>%
+    modelr::add_predictions(model = fit.6, var = "mcevoy.fit.6") %>%
+    modelr::add_predictions(model = fit.7, var = "mcevoy.fit.7") %>%
+    modelr::add_predictions(model = fit.8, var = "mcevoy.fit.8")
+
+  mcevoy.temp <- data.frame(
+    map.id = data_w_fit.df$map.id,
+    epoch = data_w_fit.df$epoch,
+    stringsAsFactors = FALSE
+  )
+
+  mcevoy.temp$fit.1 <-
+    ((data_w_fit.df[, mcevoy.Y[1]] - data_w_fit.df$mcevoy.fit.1) / sd(resid(fit.1), na.rm = TRUE)) * mcevoy.coef[1]
+  mcevoy.temp$fit.2 <-
+    ((data_w_fit.df[, mcevoy.Y[2]] - data_w_fit.df$mcevoy.fit.2) / sd(resid(fit.2), na.rm = TRUE)) * mcevoy.coef[2]
+  mcevoy.temp$fit.3 <-
+    ((data_w_fit.df[, mcevoy.Y[3]] - data_w_fit.df$mcevoy.fit.3) / sd(resid(fit.3), na.rm = TRUE)) * mcevoy.coef[3]
+  mcevoy.temp$fit.4 <-
+    ((data_w_fit.df[, mcevoy.Y[4]] - data_w_fit.df$mcevoy.fit.4) / sd(resid(fit.4), na.rm = TRUE)) * mcevoy.coef[4]
+  mcevoy.temp$fit.5 <-
+    ((data_w_fit.df[, mcevoy.Y[5]] - data_w_fit.df$mcevoy.fit.5) / sd(resid(fit.5), na.rm = TRUE)) * mcevoy.coef[5]
+  mcevoy.temp$fit.6 <-
+    ((data_w_fit.df[, mcevoy.Y[6]] - data_w_fit.df$mcevoy.fit.6) / sd(resid(fit.6), na.rm = TRUE)) * mcevoy.coef[6]
+  mcevoy.temp$fit.7 <-
+    ((data_w_fit.df[, mcevoy.Y[7]] - data_w_fit.df$mcevoy.fit.7) / sd(resid(fit.7), na.rm = TRUE)) * mcevoy.coef[7]
+  mcevoy.temp$fit.8 <-
+    ((data_w_fit.df[, mcevoy.Y[8]] - data_w_fit.df$mcevoy.fit.8) / sd(resid(fit.8), na.rm = TRUE)) * mcevoy.coef[8]
+
+  mcevoy.temp$AD.sig.mcevoy <- rowSums(mcevoy.temp[, c("fit.1", "fit.2", "fit.3", "fit.4", "fit.5", "fit.6", "fit.7", "fit.8")])
+
+  mcevoy.temp <- mcevoy.temp %>%
+    select(map.id, epoch, AD.sig.mcevoy)
+
+  data <- left_join(data, mcevoy.temp, by = c("map.id", "epoch"))
 
   data$AD.sig.schwarz <- rowMeans(data[, schwarz], na.rm = FALSE)
 
   data <- within(data, {
-    label(avg.hippocampus) <- "Avg hippocampus vol - T1 3T FreeSurfer"
+    label(avg.hippocampus) <- "Avg hippocampus vol - T1 3T FS"
     label(avg.entorhinal.thickness) <- "Avg entorhinal cortex thickness - T1 3T FS"
     label(avg.middletemporal.thickness) <- "Avg middletemporal thickness - T1 3T FS"
     label(avg.bankssts.thickness) <- "Avg bankssts thickness - T1 3T FS"
