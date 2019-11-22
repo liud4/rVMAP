d801b3c0e7f967bd5fa2cd520032b1fcdf19c489
Omair A. Khan <omair.adil.khan@gmail.com>
2019-11-22 09:40:35 -0600

    documentation updates

--- R/process_calculated_fields.R
+++ R/process_calculated_fields.R
@@ -1,3 +1,20 @@
+#' Invalidation of REDCap calculated fields.
+#'
+#' Numeric observations in VMAP data are coded as invalid with negative multiples of 1111 (e.g. -8888, -9999, etc.).
+#' While these observations are converted to NA using \code{missing_to_na} in the \code{process_raw_data} script,
+#' \code{missing_to_na} is unable to always detect invalid values in calculated fields from REDCap.
+#' \code{process_calculated_fields} uses regular expressions to extract which variables were utilized in the
+#' REDCap calculation of a given variable. If any of the component variables were invalidated by \code{missing_to_na},
+#' \code{process_calculated_fields} will convert the corresponding calculated values to NA.
+#'
+#' @param data A dataframe containing data from REDCap.
+#' @param data_label The label corresponding to the data as stored in MAPfreeze.list (used to locate metadata).
+#' @param epoch An integer corresponding to the time period of data (used to locate metadata).
+#' @return The input dataframe with properly invalidated observations in REDCap calculated variables.
+#' @examples
+#' apoe_processed.df <- process_calculated_fields(data = apoe.df, data_label = "APOE")
+#' @export
+
 process_calculated_fields <- function(data, data_label, epoch = current_epoch) {
   current_data.df <- data
 
