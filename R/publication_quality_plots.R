f1a8b21edc964ca9ddfa990717251d2456b02b93
Omair A. Khan <omair.adil.khan@gmail.com>
2020-07-10 12:18:13 -0500

    added cross-sectional fitted plot code

--- R/publication_quality_plots.R
+++ R/publication_quality_plots.R
@@ -1,4 +1,5 @@
 color_scale_scatter <- c("#000000", "#CC3311", "#004488", "#BBBBBB")
+color_scale_fitted <- color_scale_scatter
 color_scale_boxplot <- c("#FFFFFF", "#D76351", "#4570A1", "#C9C9C9")
 
 main_scatter <- function(data = mydat, x, y, xlab = x, ylab = y, label = NULL, return = TRUE, savePNG = FALSE, saveTIFF = FALSE) {
@@ -17,7 +18,7 @@ main_scatter <- function(data = mydat, x, y, xlab = x, ylab = y, label = NULL, r
     theme(
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
-      panel.background = element_rect(colour = 'black', fill = 'white'),
+      panel.background = element_rect(color = 'black', fill = 'white'),
       axis.title = element_text(size = 12),
       plot.title = element_text(size = 12)
     ) +
@@ -70,11 +71,11 @@ inter_scatter <- function(data = mydat, x, y, group, model = group, xlab = x, yl
     xlab(xlab) +
     ylab(ylab) +
     labs(color = grouplab) +
-    scale_colour_manual(values = color_scale_scatter) +
+    scale_color_manual(values = color_scale_scatter) +
     theme(
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
-      panel.background = element_rect(colour = 'black', fill = 'white'),
+      panel.background = element_rect(color = 'black', fill = 'white'),
       axis.title = element_text(size = 12),
       plot.title = element_text(size = 12)
     ) +
@@ -115,7 +116,7 @@ main_box <- function(data = mydat, x, y, xlab = x, ylab = y, label = NULL, retur
     theme(
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
-      panel.background = element_rect(colour = 'black', fill = 'white'),
+      panel.background = element_rect(color = 'black', fill = 'white'),
       axis.title = element_text(size = 12),
       plot.title = element_text(size = 12)
     ) +
@@ -171,7 +172,7 @@ inter_box <- function(data = mydat, x, y, group, model = group, xlab = x, ylab =
     theme(
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
-      panel.background = element_rect(colour = 'black', fill = 'white'),
+      panel.background = element_rect(color = 'black', fill = 'white'),
       axis.title = element_text(size = 12),
       plot.title = element_text(size = 12)
     ) +
@@ -198,3 +199,100 @@ inter_box <- function(data = mydat, x, y, group, model = group, xlab = x, ylab =
     return(p)
   }
 }
+
+main_fitted <- function(regression_list = cs.regression.list, model, x, y, xlab = x, ylab = y, return = TRUE, savePNG = FALSE, saveTIFF = FALSE) {
+
+  filename.str <- paste0("fitted_", "main", "_", x, "_", y)
+
+  fit_obj <- regression_list[[x]][["main"]][[y]]
+
+  options(datadist = NULL)
+
+  dd <<- fit_obj$dd
+
+  options(datadist = "dd")
+
+  temp.list <- list(fit_obj$fit, x)
+  prediction.df <- data.frame(do.call(rms::Predict, temp.list))
+
+  p <- ggplot(data = prediction.df, aes_string(x = x, y = "yhat")) +
+    geom_line(size = 0.9, color = 'black') +
+    geom_ribbon(aes_string(ymin = 'lower', ymax = 'upper'), alpha = 0.2, color = 'black') +
+    scale_x_continuous(breaks = scales::breaks_pretty()) +
+    scale_y_continuous(breaks = scales::breaks_pretty()) +
+    xlab(xlab) +
+    ylab(ylab) +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(color = 'black', fill = 'white'),
+      axis.title = element_text(size = 12),
+      plot.title = element_text(size = 12)
+    ) +
+    theme(aspect.ratio = 1)
+
+  options(datadist = NULL)
+
+  if (isTRUE(savePNG)) {
+    ggsave(filename = file.path(proj.graphics.dir, todays_date(), paste0(filename.str, ".png")), plot = p)
+  }
+
+  if (isTRUE(saveTIFF)) {
+    ggsave(filename = file.path(proj.graphics.dir, todays_date(), paste0(filename.str, ".tiff")), plot = p)
+  }
+
+  if (isTRUE(return)) {
+    return(p)
+  }
+
+}
+
+inter_fitted <- function(regression_list = cs.regression.list, model, x, y, group, xlab = x, ylab = y, grouplab = group, return = TRUE, savePNG = FALSE, saveTIFF = FALSE) {
+
+  filename.str <- paste0("fitted_", model, "_", x, "_", y)
+
+  fit_obj <- regression_list[[x]][[model]][[y]]
+
+  options(datadist = NULL)
+
+  dd <<- fit_obj$dd
+
+  options(datadist = "dd")
+
+  temp.list <- list(fit_obj$fit, x, group)
+  prediction.df <- data.frame(do.call(rms::Predict, temp.list))
+
+  p <- ggplot(data = prediction.df, aes_string(x = x, y = "yhat")) +
+    geom_line(aes_string(color = group), size = 0.9) +
+    geom_ribbon(aes_string(ymin = 'lower', ymax = 'upper', fill = group), alpha = 0.2) +
+    scale_x_continuous(breaks = scales::breaks_pretty()) +
+    scale_y_continuous(breaks = scales::breaks_pretty()) +
+    xlab(xlab) +
+    ylab(ylab) +
+    scale_color_manual(name = grouplab, values = color_scale_fitted) +
+    scale_fill_manual(name = grouplab, values = color_scale_fitted) +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_rect(color = 'black', fill = 'white'),
+      axis.title = element_text(size = 12),
+      plot.title = element_text(size = 12)
+    ) +
+    theme(aspect.ratio = 1)
+
+  options(datadist = NULL)
+
+  if (isTRUE(savePNG)) {
+    ggsave(filename = file.path(proj.graphics.dir, todays_date(), paste0(filename.str, ".png")), plot = p)
+  }
+
+  if (isTRUE(saveTIFF)) {
+    ggsave(filename = file.path(proj.graphics.dir, todays_date(), paste0(filename.str, ".tiff")), plot = p)
+  }
+
+  if (isTRUE(return)) {
+    return(p)
+  }
+
+}
+
