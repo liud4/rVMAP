d05a4bb90c1fbd0c6c15eb3a66f479ac1e216735
Omair A. Khan <omair.adil.khan@gmail.com>
2021-05-25 09:57:27 -0500

    fixes color blind code by keeping only underscore variable names

--- R/invalidate_color_blind.R
+++ R/invalidate_color_blind.R
@@ -5,9 +5,11 @@
 #' @export
 
 invalidate_color_blind <- function(data) {
+  data <- merged.df
+
   color_blind_id.list <- c("028", "033", "131", "169", "176", "201")
 
-  color_blind_underscore.var <- Hmisc::Cs(
+  color_blind.var <- Hmisc::Cs(
     np_color,
     np_color_ss,
     np_word,
@@ -48,52 +50,7 @@ invalidate_color_blind <- function(data) {
     np_strp_scerr_elig
   )
 
-  color_blind_period.var <- Hmisc::Cs(
-    np.color,
-    np.color.ss,
-    np.word,
-    np.word.ss,
-    np.inhibit,
-    np.inhibit.ss,
-    np.colorword.sum,
-    np.colorword.comp,
-    np.inhibitcolor.diff,
-    np.inhibitcolor.contrast,
-    np.color.scerr,
-    np.color.ucerr,
-    np.color.err,
-    np.color.cumpercerr,
-    np.word.scerr,
-    np.word.ucerr,
-    np.word.err,
-    np.word.cumpercerr,
-    np.inhibit.scerr,
-    np.inhibit.cumpercscerr,
-    np.inhibit.ucerr,
-    np.inhibit.cumpercucerr,
-    np.inhibit.err,
-    np.inhibit.err.ss,
-    np.strp.word.elig,
-    np.strp.word.ss.elig,
-    np.strp.word.ucerr.elig,
-    np.strp.word.scerr.elig,
-    np.strp.color.elig,
-    np.strp.color.ss.elig,
-    np.strp.color.ucerr.elig,
-    np.strp.color.scerr.elig,
-    np.strp.colorword.elig,
-    np.strp.colorword.ss.elig,
-    np.strp.colorword.ucerr.elig,
-    np.strp.colorword.scerr.elig,
-    np.strp.ucerr.elig,
-    np.strp.scerr.elig
-  )
-
-  color_blind.var <- intersect(names(data), c(color_blind_underscore.var, color_blind_period.var))
-
-  if (length(color_blind.var) > 0) {
-    data[data$map.id %in% color_blind_id.list, color_blind.var] <- NA
-  }
+  data[data$map_id %in% color_blind_id.list, color_blind.var] <- NA
 
   return(data)
 }
