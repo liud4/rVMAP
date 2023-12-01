516be616aa128cde1be7e4b9c50c83e78029564a
Zhang <panpan.zhang@vumc.org>
2023-12-01 12:18:37 -0600

    update derive_demographics (for age)

--- R/derive_demographics.R
+++ R/derive_demographics.R
@@ -6,40 +6,42 @@
 
 derive_demographics <- function(data) {
   days_in_one_year <- 365.25
-
+  
   data <- within(data, {
-    raceethnicity <- ifelse(
-      is.na(race) | is.na(ethnicity),
-      NA,
-      ifelse(
-        race == 1 & ethnicity == 0,
-        1,
-        ifelse(
-          (race %in% c(0, 2, 3, 4) & ethnicity %in% c(0, 1)) | (race == 1 & ethnicity == 1),
-          2,
-          NA
-        )
-      )
-    )
-
+    raceethnicity <- ifelse(is.na(race) | is.na(ethnicity),
+                            NA,
+                            ifelse(race == 1 & ethnicity == 0,
+                                   1,
+                                   ifelse(
+                                     (race %in% c(0, 2, 3, 4) &
+                                        ethnicity %in% c(0, 1)) | (race == 1 & ethnicity == 1),
+                                     2,
+                                     NA
+                                   )))
+    
     label(raceethnicity) <- "Two-level race/ethnicity"
-
+    
     raceethnicity.factor <- factor(
       raceethnicity,
       levels = 1:2,
       labels = c("Non-Hispanic White", "Other")
     )
-
+    
     label(raceethnicity.factor) <- "Two-level race/ethnicity"
-
+    
     age.redcap <- age
-
-    age <- floor(
-      as.numeric(difftime(medhx.date, dob, units = "days")) / days_in_one_year
-    )
-
+    
+    if ((!is.na(vf_arrival_date_time)) &
+        (!(is.numeric(map.id) <= 336 & epoch == 1))) {
+      age <-
+        floor(
+          as.numeric(difftime(vf_arrival_date_time, dob, units = "days")) / days_in_one_year
+    } else {
+      age <- floor(as.numeric(difftime(medhx.date, dob, units = "days")) / days_in_one_year)
+    }
+    
     label(age) <- "Age at medhx.date, recalculated"
   })
-
-  return(data)
+      
+      return(data)
 }
