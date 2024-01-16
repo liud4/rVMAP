e8082b21711adb82b9a3249f16b08639e33e4c56
Zhang <panpan.zhang@vumc.org>
2024-01-16 15:56:32 -0600

    update derive_informant_info

--- R/derive_informant_info.R
+++ R/derive_informant_info.R
@@ -9,6 +9,8 @@ derive_informant_info <- function(data) {
     # convenience var for use in calculation of inform.length.totyrs
     # AJ confirmed in email 23 Feb 2015 that she wants to impute 0 for mos
     # in cases where yrs is nonmissing but mos is missing
+    inform.length.mos <- as.numeric(inform.length.mos) ## "inform.length.mos" becomes a labeled character
+    label(inform.length.mos) <- "Length of Time Informant Has Known Participant - months"
     inform.length.imputedmosInYrs <- ifelse(
       is.na(inform.length.yrs) & is.na(inform.length.mos), NA,
       ifelse(
