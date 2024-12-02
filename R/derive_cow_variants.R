3faf9b864a58235154c73ac095e8b079afc67820
Zhang <panpan.zhang@vumc.org>
2024-12-02 14:53:07 -0600

    update cow derivation function

--- R/derive_cow_variants.R
+++ R/derive_cow_variants.R
@@ -7,148 +7,141 @@
 derive_cow_variants <- function(data) {
   data <- data %>%
     mutate(
-      cow.variant2 = case_when( # derive: cow.variant2, CoW variant Rivera-Rivera
-        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, textbook
-        cow.variant %in% c(10, 11, 21, 22, 25, 26, 29, 30) ~ 1, # 1, A1 hypoplasia
-        cow.variant %in% c(12, 13, 23, 24, 27, 28, 31, 32, 35) ~ 2, # 2, P1 hypoplasia
-        cow.variant %in% c(14, 15, 16, 17, 18, 19, 20, 33, 34, 36, 37, 38) ~ 3, # 3, Other
+      cow.variant.archive1 = case_when( # previous: cow.variant2; CoW variant Rivera-Rivera; Variant A1s or P1s
+        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, Textbook
+        cow.variant %in% c(10, 11, 21, 22, 25, 26, 29, 30) ~ 1, # 1, A1 variant unilateral
+        cow.variant %in% c(12, 13, 14, 23, 24, 27, 28, 31, 32, 35) ~ 2, # 2, P1 variant unilateral
+        cow.variant %in% c(15, 16, 17, 18, 19, 20, 33, 34, 36, 37, 38) ~ 3, # 3, Other
         TRUE ~ NA_real_
       ),
-      cow.variant3 = case_when( # derive: cow.variant3, CoW variant Ryan
-        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Fully patent A1s and P1s
-        (cow.a1.l %in% 3 | cow.a1.r %in% 3) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 1, # 1, A1 missing
-        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & (cow.p1.l %in% 3 | cow.p1.r %in% 3) ~ 2, # 2, P1 missing bilateral/unilateral
+      cow.variant.missing.a1.p1 = case_when( # previous: cow.variant3; CoW variant Ryan; Missing A1s or P1s
+        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Patent A1s and P1s
+        (cow.a1.l %in% 3 | cow.a1.r %in% 3) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 1, # 1, A1 missing unilateral/bilateral
+        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & (cow.p1.l %in% 3 | cow.p1.r %in% 3) ~ 2, # 2, P1 missing unilateral/bilateral
+        (cow.a1.l %in% 3 | cow.a1.r %in% 3) & (cow.p1.l %in% 3 | cow.p1.r %in% 3) ~ 3, # 3, A1 and P1 missing unilateral/bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant4 = case_when( # derive: cow.variant4, CoW variant Vrselja
-        cow.acoa %in% c(1, 2) & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% c(1, 2) ~ 0, # 0, Fully patent AcoA and PcoAs
+      cow.variant.missing.acoa.pcoa = case_when( # previous: cow.variant4; CoW variant Vrselja; Missing AcoA or PcoAs
+        cow.acoa %in% c(1, 2) & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% c(1, 2) ~ 0, # 0, Patent AcoA and PcoAs
         cow.acoa %in% 3 & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% c(1, 2) ~ 1, # 1, AcoA missing
-        cow.acoa %in% c(1, 2) & (cow.pcoa.l %in% 3 | cow.pcoa.r %in% 3) ~ 2, # 2, PcoA missing bilateral/unilateral
-        cow.acoa %in% 3 & (cow.pcoa.l %in% 3 | cow.pcoa.r %in% 3) ~ 3, # 3, AcoA and PcoA missing bilateral/unilateral
+        cow.acoa %in% c(1, 2) & (cow.pcoa.l %in% 3 | cow.pcoa.r %in% 3) ~ 2, # 2, PcoA missing unilateral/bilateral
+        cow.acoa %in% 3 & (cow.pcoa.l %in% 3 | cow.pcoa.r %in% 3) ~ 3, # 3, AcoA and PcoA missing unilateral/bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant5 = case_when( # derive: cow.variant5, COW variant A1 Left P1 hypoplasia bilateral included
-        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, textbook
-        cow.variant %in% c(10, 21, 25, 29) ~ 1, # 1, Left A1 hypoplasia
-        cow.variant %in% c(11, 22, 26, 30) ~ 2, # 2, Right A1 hypoplasia
-        cow.variant %in% c(12, 14, 23, 28, 32, 35) ~ 3, # 3, Left P1 hypoplasia (with bilateral)
+      cow.variant.archive2 = case_when( # previous: cow.variant5; Variant A1s or P1s, left vs. right, grouping Bilateral P1 with Left P1
+        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, Textbook
+        cow.variant %in% c(10, 21, 25, 29) ~ 1, # 1, Left A1 variant
+        cow.variant %in% c(11, 22, 26, 30) ~ 2, # 2, Right A1 variant
+        cow.variant %in% c(12, 14, 23, 28, 32, 35) ~ 3, # 3, Left P1 variant (with bilateral)
         cow.variant %in% c(13, 24, 15, 16, 17, 18, 19, 20, 33, 34, 36, 37, 38, 27, 31) ~ 4, # 4, Other
         TRUE ~ NA_real_
       ),
-      cow.variant6 = case_when( # derive: cow.variant5, COW variant A1 Right P1 hypoplasia bilateral included
-        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, textbook
-        cow.variant %in% c(10, 21, 25, 29) ~ 1, # 1, Left A1 hypoplasia
-        cow.variant %in% c(11, 22, 26, 30) ~ 2, # 2, Right A1 hypoplasia
-        cow.variant %in% c(13, 14, 24, 27, 31, 35) ~ 3, # 3, Right P1 hypoplasia (with bilateral)
+      cow.variant.archive3 = case_when( # previous: cow.variant6; Variant A1s or P1s, left vs. right, grouping Bilateral P1 with Right P1
+        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, Textbook
+        cow.variant %in% c(10, 21, 25, 29) ~ 1, # 1, Left A1 variant
+        cow.variant %in% c(11, 22, 26, 30) ~ 2, # 2, Right A1 variant
+        cow.variant %in% c(13, 14, 24, 27, 31, 35) ~ 3, # 3, Right P1 variant (with bilateral)
         cow.variant %in% c(12, 23, 15, 16, 17, 18, 19, 20, 33, 34, 36, 37, 38, 28, 32) ~ 4, # 4, Other
         TRUE ~ NA_real_
       ),
-      cow.variant7 = case_when( # derive: cow.variant5, COW variant A1 P1 hypoplasia bilateral separate
-        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, textbook
-        cow.variant %in% c(10, 21, 25, 29) ~ 1, # 1, Left A1 hypoplasia
-        cow.variant %in% c(11, 22, 26, 30) ~ 2, # 2, Right A1 hypoplasia
-        cow.variant %in% c(12, 23, 28, 32) ~ 3, # 3, Left P1 hypoplasia (without bilateral)
-        cow.variant %in% c(13, 24, 27, 31) ~ 4, # 4, Right P1 hypoplasia (without bilateral)
-        cow.variant %in% c(14, 35) ~ 5, # 5, Bilateral P1 hypoplasia
+      cow.variant.archive4 = case_when( # previous: cow.variant7; Variant A1s or P1s, left vs. right, grouping Bilateral P1 separately
+        cow.variant %in% c(1, 3, 4, 5, 6, 7, 8, 9) ~ 0, # 0, Textbook
+        cow.variant %in% c(10, 21, 25, 29) ~ 1, # 1, Left A1 variant
+        cow.variant %in% c(11, 22, 26, 30) ~ 2, # 2, Right A1 variant
+        cow.variant %in% c(12, 23, 28, 32) ~ 3, # 3, Left P1 variant (without bilateral)
+        cow.variant %in% c(13, 24, 27, 31) ~ 4, # 4, Right P1 variant (without bilateral)
+        cow.variant %in% c(14, 35) ~ 5, # 5, Bilateral P1 variant
         cow.variant %in% c(15, 16, 17, 18, 19, 20, 33, 34, 36, 37, 38) ~ 6, # 6, Other
         TRUE ~ NA_real_
       ),
-      cow.variant8 = case_when( # derive: cow.variant8, COW variant A1 Left P1 missing bilateral included
-        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Fully patent A1s and P1s
+      cow.variant.archive5 = case_when( # previous: cow.variant8; Missing A1s or P1s, left vs. right, grouping Bilateral with Left P1
+        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Patent A1s and P1s
         cow.a1.l %in% 3 & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 1, # 1, Left A1 missing
         cow.a1.l %in% c(1, 2) & cow.a1.r %in% 3 & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 2, # 2, Right A1 missing
         cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% 3 & cow.p1.r %in% c(1, 2, 3) ~ 3, # 3, Left P1 missing (with bilateral)
         cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% 3 ~ 4, # 4, Right P1 missing (without bilateral)
         TRUE ~ NA_real_
       ),
-      cow.variant9 = case_when( # derive: cow.variant9, COW variant A1 Right P1 missing bilateral included
-        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Fully patent A1s and P1s
+      cow.variant.archive6 = case_when( # previous: cow.variant9; Missing A1s or P1s, left vs. right, grouping Bilateral with Right P1
+        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Patent A1s and P1s
         cow.a1.l %in% 3 & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 1, # 1, Left A1 missing
         cow.a1.l %in% c(1, 2) & cow.a1.r %in% 3 & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 2, # 2, Right A1 missing
-        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2, 3) & cow.p1.r %in% 3 ~ 3, # 3, Right P1 missing (with bilateral)
-        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% 3 & cow.p1.r %in% c(1, 2) ~ 4, # 4, Left P1 missing (without bilateral)
+        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% 3 & cow.p1.r %in% c(1, 2) ~ 3, # 3, Left P1 missing (without bilateral)
+        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2, 3) & cow.p1.r %in% 3 ~ 4, # 4, Right P1 missing (with bilateral)
         TRUE ~ NA_real_
       ),
-      cow.variant10 = case_when( # derive: cow.variant10, COW variant AcoA PcoA missing
-        cow.acoa %in% c(1, 2) & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% c(1, 2) ~ 0, # 0, Fully patent AcoA and PcoAs
+      cow.variant.missing.acoa.pcoa.lr = case_when( # previous: cow.variant10; Missing AcoA or PcoAs, left vs. right
+        cow.acoa %in% c(1, 2) & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% c(1, 2) ~ 0, # 0, Patent AcoA and PcoAs
         cow.acoa %in% 3 & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% c(1, 2) ~ 1, # 1, AcoA missing
         cow.acoa %in% c(1, 2) & cow.pcoa.l %in% 3 & cow.pcoa.r %in% c(1, 2) ~ 2, # 2, Left PcoA missing
         cow.acoa %in% c(1, 2) & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% 3 ~ 3, # 3, Right PcoA missing
         cow.acoa %in% 3 & cow.pcoa.l %in% 3 & cow.pcoa.r %in% c(1, 2) ~ 4, # 4, AcoA and Left PcoA missing
         cow.acoa %in% 3 & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% 3 ~ 5, # 5, AcoA and Right PcoA missing
-        cow.acoa %in% 3 & cow.pcoa.l %in% 3 & cow.pcoa.r %in% 3 ~ 6, # 6, AcoA, Left PcoA, and Right PcoA missing
+        cow.acoa %in% c(1, 2) & cow.pcoa.l %in% 3 & cow.pcoa.r %in% 3 ~ 6, # 6, Left PcoA and Right PcoA missing
+        cow.acoa %in% 3 & cow.pcoa.l %in% 3 & cow.pcoa.r %in% 3 ~ 7, # 7, Left PcoA, Right PcoA and AcoA missing
         TRUE ~ NA_real_
       ),
-      cow.variant11 = case_when( # derive: cow.variant11, COW variant Missing A1
-        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) & cow.p1.l %in% c(1, 2, 3) & cow.p1.r %in% c(1, 2, 3) ~ 0, # 0, Both A1s Present
-        (cow.a1.l %in% 3 | cow.a1.r %in% 3) & cow.p1.l %in% c(1, 2, 3) & cow.p1.r %in% c(1, 2, 3) ~ 1, # 1, A1 Missing
+      cow.variant.missing.a1 = case_when( # previous: cow.variant11; Missing A1s
+        cow.a1.l %in% c(1, 2) & cow.a1.r %in% c(1, 2) ~ 0, # 0, Patent A1s
+        cow.a1.l %in% 3 | cow.a1.r %in% 3 ~ 1, # 1, A1 Missing unilateral/bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant12 = case_when( # derive: cow.variant12, COW variant Missing P1
-        cow.a1.l %in% c(1, 2, 3) & cow.a1.r %in% c(1, 2, 3) &  cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Both P1s Present
-        cow.a1.l %in% c(1, 2, 3) & cow.a1.r %in% c(1, 2, 3) & (cow.p1.l %in% 3 | cow.p1.r %in% 3) ~ 1, # 1, P1 Missing
+      cow.variant.missing.p1 = case_when( # previous: cow.variant12; Missing P1s (Full FTP)
+        cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Patent P1s
+        cow.p1.l %in% 3 | cow.p1.r %in% 3 ~ 1, # 1, P1 Missing unilateral/bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant13 = case_when( # derive: cow.variant13, COW variant Missing P1 Left
-        cow.a1.l %in% c(1, 2, 3) & cow.a1.r %in% c(1, 2, 3) & cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2, 3) ~ 0, # 0, Left P1 Present
-        cow.a1.l %in% c(1, 2, 3) & cow.a1.r %in% c(1, 2, 3) & cow.p1.l %in% 3 & cow.p1.r %in% c(1, 2, 3) ~ 1, # 1, Left P1 Missing
+      cow.variant.archive7 = case_when( # previous: cow.variant13; Missing Left P1
+        cow.p1.l %in% c(1, 2) ~ 0, # 0, Pantent Left P1
+        cow.p1.l %in% 3 ~ 1, # 1, Left P1 Missing
         TRUE ~ NA_real_
       ),
-      cow.variant14 = case_when(
+      cow.variant.hypoplastic.acoa.pcoa = case_when( # previous: cow.variant14; Hypoplastic AcoA or PcoAs
         cow.acoa %in% 1 & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 1 ~ 0, # 0, Normal AcoA and PcoAs
         cow.acoa %in% 2 & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 1 ~ 1, # 1, AcoA hypoplasia
-        cow.acoa %in% 1 & (cow.pcoa.l %in% 2 | cow.pcoa.r %in% 2) ~ 2, # 2, PcoA hypoplasia bilateral/unilateral
-        cow.acoa %in% 2 & (cow.pcoa.l %in% 2 | cow.pcoa.r %in% 2) ~ 3, # 3, AcoA and PcoA hypoplasia bilateral/unilateral
-        cow.acoa %in% 3 | cow.pcoa.l %in% 3 | cow.pcoa.r %in% 3 ~ 4, # 4, Other
+        cow.acoa %in% 1 & (cow.pcoa.l %in% 2 | cow.pcoa.r %in% 2) ~ 2, # 2, PcoA hypoplasia unilateral/bilateral
+        cow.acoa %in% 2 & (cow.pcoa.l %in% 2 | cow.pcoa.r %in% 2) ~ 3, # 3, AcoA and PcoA hypoplasia unilateral/bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant15 = case_when(
+      cow.variant.hypoplastic.acoa.pcoa.lr = case_when( # previous: cow.variant15; Hypoplastic AcoA or PcoAs, left vs. right
         cow.acoa %in% 1 & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 1 ~ 0, # 0, Normal AcoA and PcoAs
         cow.acoa %in% 2 & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 1 ~ 1, # 1, AcoA hypoplasia
         cow.acoa %in% 1 & cow.pcoa.l %in% 2 & cow.pcoa.r %in% 1 ~ 2, # 2, Left PcoA hypoplasia
         cow.acoa %in% 1 & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 2 ~ 3, # 3, Right PcoA hypoplasia
         cow.acoa %in% 2 & cow.pcoa.l %in% 2 & cow.pcoa.r %in% 1 ~ 4, # 4, Left PcoA and AcoA hypoplasia
-        cow.acoa %in% 2 & cow.pcoa.l %in% c(1, 2) & cow.pcoa.r %in% 2 ~ 5, # 5, Right PcoA and AcoA hypoplasia
+        cow.acoa %in% 2 & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 2 ~ 5, # 5, Right PcoA and AcoA hypoplasia
         cow.acoa %in% 1 & cow.pcoa.l %in% 2 & cow.pcoa.r %in% 2 ~ 6, # 6, Left PcoA and Right PcoA hypoplasia
         cow.acoa %in% 2 & cow.pcoa.l %in% 2 & cow.pcoa.r %in% 2 ~ 7, # 7, Left PcoA, Right PcoA, and AcoA hypoplasia
-        cow.acoa %in% 3 | cow.pcoa.l %in% 3 | cow.pcoa.r %in% 3 ~ 8, # 8, Other
         TRUE ~ NA_real_
       ),
-      cow.variant16 = case_when(
+      cow.variant.missing.hyppoplastic.acoa.pcoa = case_when( # previous: cow.variant16; Missing or Hypoplastic AcoA or PcoAs
         cow.acoa %in% 1 & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 1 ~ 0, # 0, Normal AcoA and PcoAs
         cow.acoa %in% c(2, 3) & cow.pcoa.l %in% 1 & cow.pcoa.r %in% 1 ~ 1, # 1, AcoA hypoplasia or missing
-        cow.acoa %in% 1 & (cow.pcoa.l %in% c(2, 3) | cow.pcoa.r %in% c(2, 3)) ~ 2, # 2, PcoA hypoplasia or missing bilateral/unilateral
-        cow.acoa %in% c(2, 3) | cow.pcoa.l %in% c(2, 3) | cow.pcoa.r %in% c(2, 3) ~ 3, # 3, AcoA and PcoA hypoplasia or missing bilateral/unilateral,
+        cow.acoa %in% 1 & (cow.pcoa.l %in% c(2, 3) | cow.pcoa.r %in% c(2, 3)) ~ 2, # 2, PcoA hypoplasia or missing unilateral/bilateral
+        cow.acoa %in% c(2, 3) & (cow.pcoa.l %in% c(2, 3) | cow.pcoa.r %in% c(2, 3)) ~ 3, # 3, AcoA and PcoA missing or hypoplasia unilateral/bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant17 = case_when(
+      cow.variant.missing.p1.ub = case_when( # previous: cow.variant17; Missing P1s, unilateral vs. bilateral
         cow.p1.l %in% c(1, 2) & cow.p1.r %in% c(1, 2) ~ 0, # 0, Normal circle
-        (cow.p1.l %in% 3 & cow.p1.r %in% c(1, 2)) | (cow.p1.l %in% c(1, 2) & cow.p1.r %in% 3) ~ 1, # 1, Unilateral P1 missing
-        cow.p1.l %in% 3 & cow.p1.r %in% 3 ~ 0, # 2, Bilateral P1 missing
+        (cow.p1.l %in% 3 & cow.p1.r %in% c(1, 2)) | (cow.p1.l %in% c(1, 2) & cow.p1.r %in% 3) ~ 1, # 1, P1 missing unilateral
+        cow.p1.l %in% 3 & cow.p1.r %in% 3 ~ 0, # 2, P1 missing bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant18 = case_when(
+      cow.variant.partialftp = case_when( # previous: cow.variant18; Partial FTP, left vs. right
         cow.p1.l %in% 1 & cow.p1.r %in% 1 ~ 0, # 0, Normal circle
         cow.p1.l %in% 2 & cow.pcoa.l %in% 1 & cow.p1.r %in% 1 ~ 1, # 1, Left P1 hypoplasia, Left PcoA normal
-        cow.p1.r %in% 2 & cow.pcoa.l %in% 1 & cow.p1.r %in% 1 ~ 2, # 2, Right P1 hypoplasia, Right PcoA normal
+        cow.p1.r %in% 2 & cow.pcoa.r %in% 1 & cow.p1.l %in% 1 ~ 2, # 2, Right P1 hypoplasia, Right PcoA normal
         cow.p1.l %in% 2 & cow.pcoa.l %in% 1 & cow.p1.r %in% 2 & cow.pcoa.r %in% 1 ~ 3, # 3, Bilateral P1 hypoplasia (relative to PcoA)
-        cow.p1.l %in% 3 | cow.p1.r %in% 3 ~ 4, # 4, Other
         TRUE ~ NA_real_
       ),
-      cow.variant19 = case_when(
+      cow.variant.partialftp.ub = case_when( # previous: cow.variant19; Partial FTP
         cow.p1.l %in% 1 & cow.p1.r %in% 1 ~ 0, # 0, Normal circle
-        cow.p1.l %in% 2 & cow.pcoa.l %in% 1 & cow.p1.r %in% 1 ~ 1, # 1, (Left P1 hypoplasia, Left PcoA normal)
-        cow.p1.r %in% 2 & cow.pcoa.r %in% 1 & cow.p1.l %in% 1 ~ 1, # 1, (Right P1 hypoplasia, Right PcoA normal)
-        cow.p1.l %in% 2 & cow.pcoa.l %in% 1 & cow.p1.r %in% 2 & cow.pcoa.r %in% 1 ~ 1, # 1, (Bilateral P1 hypoplasia (relative to PcoA),
+        (cow.p1.l %in% 2 & cow.pcoa.l %in% 1) | (cow.p1.r %in% 2 & cow.pcoa.r %in% 1) ~ 1, # 1, Partial FTP unilateral/bilateral
         TRUE ~ NA_real_
       ),
-      cow.variant20 = case_when(
+      ## !!DO NOT RUN; PUSH for records.
+      cow.variant.missing.p1.partialftp = case_when( # previous: cow.variant20; Full or Partial FTP 
         cow.p1.l %in% 1 & cow.p1.r %in% 1 ~ 0, # 0, Normal circle
-        cow.p1.l %in% 3 & cow.p1.r %in% c(1, 2) ~ 1, # 1, Unilateral or Bilateral Full FTP or Unilateral or Bilateral Partial FTP
-        cow.p1.l %in% c(1, 2) & cow.p1.r %in% 3 ~ 1, # 1, Unilateral or Bilateral Full FTP or Unilateral or Bilateral Partial FTP
-        cow.p1.l %in% 3 & cow.p1.r %in% 3 ~ 1, # 1, Unilateral or Bilateral Full FTP or Unilateral or Bilateral Partial FTP
-        cow.p1.l %in% 2 & cow.pcoa.l %in% 1 & cow.p1.r %in% 1 ~ 1, # 1, Unilateral or Bilateral Full FTP or Unilateral or Bilateral Partial FTP
-        cow.p1.r %in% 2 & cow.pcoa.r %in% 1 & cow.p1.l %in% 1 ~ 1, # 1, Unilateral or Bilateral Full FTP or Unilateral or Bilateral Partial FTP
         cow.p1.l %in% 2 & cow.pcoa.l %in% 1 & cow.p1.r %in% 2 & cow.pcoa.r %in% 1 ~ 1, # 1, Unilateral or Bilateral Full FTP or Unilateral or Bilateral Partial FTP
         TRUE ~ NA_real_
       ),
@@ -238,14 +231,15 @@ derive_cow_variants <- function(data) {
     )
 
     cow.variant10 <- factor(cow.variant10,
-                            levels = c(0, 1, 2, 3, 4, 5, 6),
+                            levels = c(0, 1, 2, 3, 4, 5, 6, 7),
                             labels = c("Fully patent AcoA and PcoAs",
                                        "AcoA missing",
                                        "Left PcoA missing",
                                        "Right PcoA missing",
                                        "AcoA and Left PcoA missing",
                                        "AcoA and Right PcoA missing",
-                                       "AcoA, Left PcoA, and Right PcoA missing")
+                                       "Left PcoA and Right PcoA missing",
+                                       "Left PcoA, Right PcoA, and AcoA missing")
     )
 
     cow.variant11 <- factor(cow.variant11,
@@ -316,7 +310,7 @@ derive_cow_variants <- function(data) {
     cow.variant19 <- factor(cow.variant19,
                             levels = c(0, 1),
                             labels = c("Normal circle",
-                                       "Partial FTP")
+                                       "Unilateral or Bilateral Partial FTP")
     )
 
     cow.variant20 <- factor(cow.variant20,
